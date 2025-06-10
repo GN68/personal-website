be55a614ab71f32690689cb6b1f13917211d775e
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-06-11 05:10:02 +0800

    cool new hero

--- src/components/HomeHero.vue
+++ src/components/HomeHero.vue
@@ -2,34 +2,55 @@
 
 import ContentPanel from './ContentPanel.vue';
 import AppIconLink from './AppIconLink.vue';
-import RandomText from './RandomText.vue';
 
 </script>
 <template>
-  <ContentPanel>
-      <div class="hero-text-container">
-      <h1>Hi! I am <span class="highlight">GN</span></h1>
-      <RandomText class="quote" :lines="[
-        'Welcome to my secret lair! muwahahaha',
-        'The right man in the wrong place can make all the BBQ in the world',
-        'Stand in the ashes of a trillion dead souls, and asks the ghosts if honor matters. The silence is your answer',
-        'Bring me a bucket, and I\'ll show you a bucket',
-        'it\'s not out of the question that you might have a very minor case of serious brain damage. But don\'t be alarmed, alright? Although, if you do feel alarm, try to hold onto that feeling because that is the proper reaction to being told you have brain damage.',
-        'Nothing is more badass than treating a woman with respect!',
-        'Thank you Mario! But our Princess is in another castle!',
-        'Snake? Snake? SNAAAAAAAAKE!!!',
-        'Science isn\'t about why! It’s about why not!',
-        ]" />
-      <div class="socials">
-        <AppIconLink icon="material-symbols:chat-info" title="About" to="/about" />
-        <AppIconLink icon="uil:link" title="Links" to="/about" />
-        <AppIconLink icon="fluent:chat-12-filled" title="Projects" to="/gallery" />
+  <div>
+    <video class="hero-video" src="@/assets/hero.webm" autoplay muted></video>
+    <ContentPanel :width="50">
+        <div class="hero-text-container">
+        <h1>Hi! I am <span class="highlight">GN</span></h1>
+        <p>Programming is my passion</p>
+        <div class = "warning">This website is under construction!</div>
+        <div class="socials">
+          <AppIconLink icon="material-symbols:chat-info" title="About" to="/about" />
+          <AppIconLink icon="uil:link" title="Links" to="/about" />
+          <AppIconLink icon="fluent:chat-12-filled" title="Projects" to="/gallery" />
+        </div>
       </div>
-    </div>
-  </ContentPanel>
+    </ContentPanel>
+  </div>
 </template>
 
 <style scoped>
+
+.warning {
+  border-radius: 2px;
+  padding: 4px 4px;
+  background-color: rgba(255, 213, 0, 0.142);
+  display: flex;
+  flex-direction: column;
+  border-left: 4px solid rgb(255, 213, 0);
+  width: fit-content;
+}
+
+.warning::before {
+  content: '⚠️ Warning';
+  color: rgb(255, 213, 0);
+}
+
+.hero-video {
+  pointer-events: none;
+  user-select: none;
+  max-width: 100%;
+  object-fit: cover;
+  height: 100%;
+  
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+}
 
 .highlight {
   color: var(--primary);
@@ -46,6 +67,7 @@ h1 {
   font-size: 5rem;
   font-weight: normal;
   margin: 0;
+  border-bottom: none;
   line-height: 5rem;
 }
 
@@ -66,7 +88,7 @@ h1 {
   box-sizing: border-box;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: left;
   text-align: left;
   text-shadow: 0 0 2rem rgba(0, 0, 0, 0.8), 0 0 0.5rem rgba(0, 0, 0, 0.2);
 }
